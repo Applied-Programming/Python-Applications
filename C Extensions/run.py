a11b393bf08f34c5f599fa40dbca9eddc653426c
Aniruddha Tapas <aniruddhastapas@gmail.com>
2016-02-17 01:13:56 +0530

    Commit

---
+++ C Extensions/run.py
@@ -0,0 +1,8 @@
+import myModule
+
+print(myModule.fib(10))
+print(myModule.version())
+
+
+
+
---
+++ Regular Expressions/re1.py
@@ -0,0 +1,20 @@
+import re
+
+def Main():
+    line = "I think I understand regular expressions"
+
+    matchResult = re.match('think', line, re.M|re.I)
+    if matchResult:
+       print("Match Found: " + matchResult.group())
+    else:
+       print("No match was found")
+
+    searchResult = re.search('think', line, re.M|re.I)
+    if searchResult:
+       print("Search Found: "+searchResult.group())
+    else:
+       print("Nothing found in search")
+
+
+if __name__ == '__main__':
+    Main()
