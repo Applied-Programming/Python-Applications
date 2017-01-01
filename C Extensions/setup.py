410335f86e90eb2e835df5845b2f11dab0058287
Aniruddha Tapas <aniruddhastapas@gmail.com>
2017-01-01 23:57:45 +0530

    Delete setup.py

--- C Extensions/setup.py
+++
@@ -1,9 +0,0 @@
-//setup.py//
-from distutils.core import setup, Extension
- 
-module = Extension('myModule', sources = ['myModule.c'])
- 
-setup (name = 'PackageName',
-        version = '1.0',
-        description = 'This is a package for myModule',
-        ext_modules = [module])
